38521227b93b3fa7cf6a5a2afa9c4748813a8317
Euan Kemp <euank@euank.com>
2017-06-10 23:13:45 -0700

    add random mac function

--- util/name.sh
+++ util/name.sh
@@ -2,3 +2,13 @@
 function name::random_name() {
   sort -R "${ROOT}/util/misc_data/namelist.txt" | head -n 1
 }
+
+# https://gist.github.com/0x783czar/4115108
+function name::random_mac() {
+  hexchars="0123456789abcdef"
+  echo "24:df:86$(
+    for i in {1..6}; do
+      echo -n ${hexchars:$(( $RANDOM % 16 )):1}
+    done | sed -e 's/\(..\)/:\1/g'
+  )"
+}
