fd2fb7165644fecbefd2238fdbac7fc940ec481c
Euan Kemp <euank@euank.com>
2017-06-10 23:16:45 -0700

    Source more things

--- util/lib.sh
+++ util/lib.sh
@@ -2,9 +2,11 @@
 ROOT="$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )/.."
 
 source "${ROOT}/config.sh"
+source "${ROOT}/util/misc.sh"
+source "${ROOT}/util/libvirt/domain.sh"
+source "${ROOT}/util/certs.sh"
 source "${ROOT}/util/name.sh"
 source "${ROOT}/util/ipam.sh"
 source "${ROOT}/util/userdata.sh"
 source "${ROOT}/util/k8s.sh"
 source "${ROOT}/util/image.sh"
-
